33e208e10c8ca2b08712e182f42d6b5f3f16ae39
Konrad Wesolowski <wesolowski.konrad@outlook.com>
2023-05-03 16:46:34 +0200

    Adding new Views 'Portfolio' and 'Programming'

---
+++ Controllers/ProgrammingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ProgrammingController : Controller
+    {
+        // GET: Programming
+        public ActionResult ProgrammingMainpage()
+        {
+            return View();
+        }
+    }
+}
